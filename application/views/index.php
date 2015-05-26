bf83349203a14cccf1ed8df1aac9d0809e3a8068
Brian Wallace <blwallace@gmail.com>
2015-05-25 21:13:40 -0700

    localized css and js files

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,9 @@
 <html>
 <head>
 <title>Project Teton</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="/assets/css/bootstrap_v3_3_2.css">
 <link rel="stylesheet" type="text/css" href="/assets/css/project.css">
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-
+<script src="/assets/js/jquery_v2_1_3.js"></script>
 
     <script>
     $(document).ready(function(){
