3a8abae48936f48c00a73a55f217c124483e8878
Brian Wallace <blwallace@gmail.com>
2015-08-29 09:47:27 -0500

    added info tables to windrose

--- application/views/windrose.php
+++ application/views/windrose.php
@@ -1,10 +1,57 @@
 	
 
 	<div class="row">
-		<div class="col-xs-4"></div>
-		<div class="col-xs-4">
-			<div id="windrose_frame" class='svg-image'>	</div>
+		<div class="col-md-1"></div>
+
+		<div class="col-md-4">
+			<h4>Windrose: Wind Direction Map</h4>
+			<table class="table table-striped">
+				<tr>
+					<td>Primary Direction</td>
+					<td>NW (315), SW(120)</td>
+				</tr>				
+				<tr>
+					<td>Primary Direction</td>
+					<td>NW (315), SW(120)</td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="col-md-6">
+			<div id="windrose_frame" class='svg-image'>	</div>			
+		</div>
+
+		<div class="col-md-1"></div>		
+
+	</div>
+
+
+	<div class="row">
+		<div class="col-md-1"></div>
+
+		<div class="col-md-4">
+			<h4>Windrose: Wind Direction Map</h4>
+			<table class="table table-striped">
+				<tr>
+					<td>Highest Speed</td>
+					<td><ul>
+						<li>NW (315)</li>
+						<li>35 mph</li>
+						<li>5/15/15</li>
+						<li>Stormy</li>
+					</ul></td>
+				</tr>				
+				<tr>
+					<td>Primary Direction</td>
+					<td>NW (315), SW(120)</td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="col-md-6">			
 			<div id="speedrose_frame" class='svg-image'> </div>
 		</div>
-		<div class="col-xs-4"></div>
 
+		<div class="col-md-1"></div>		
+
+	</div>
