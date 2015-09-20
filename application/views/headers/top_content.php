67265ceb7efd8175958f80d23309094eecfcf8c4
Brian Wallace <blwallace@gmail.com>
2015-09-20 10:27:19 -0700

    trying to fix

--- application/views/headers/top_content.php
+++ application/views/headers/top_content.php
@@ -86,6 +86,7 @@ $(document).ready(function(){
           $.ajax({
             type: "POST",
             url: "/sites/locate",
+            dataType: 'json';
             data: {
                 'name': name,
                 'lat': lat,
